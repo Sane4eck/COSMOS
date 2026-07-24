9aae25220ae5c5bd03f9b5cd1247c5e300d918c0
Sane4eck <77103844+Sane4eck@users.noreply.github.com>
2026-07-24 10:00:03 +0300

    Register CSV Visualizer in COSMOS

--- apps/registry.py
+++ apps/registry.py
@@ -1,7 +1,9 @@
+from apps.CSVVisualizer.app import APP as CSVVisualizer_APP
 from apps.VitBox.app import APP as VitBox_APP
 
 
 # Додавайте сюди нові модулі.
 APPS = [
     VitBox_APP,
+    CSVVisualizer_APP,
 ]
