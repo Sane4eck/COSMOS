e8eba6fd9abf074e6c10bb17b3a0f874b242af77
Sane4eck <77103844+Sane4eck@users.noreply.github.com>
2026-07-31 11:57:19 +0300

    Register Spectrogramma in COSMOS

--- apps/registry.py
+++ apps/registry.py
@@ -1,5 +1,6 @@
 from apps.CSVVisualizer.app import APP as CSVVisualizer_APP
 from apps.HBMVisualizer.app import APP as HBMVisualizer_APP
+from apps.Spectrogramma.app import APP as Spectrogramma_APP
 from apps.VitBox.app import APP as VitBox_APP
 
 
@@ -8,4 +9,5 @@ APPS = [
     VitBox_APP,
     CSVVisualizer_APP,
     HBMVisualizer_APP,
+    Spectrogramma_APP,
 ]
